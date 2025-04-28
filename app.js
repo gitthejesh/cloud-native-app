34bba941a4e01e191978570080e30a572db965f7
gitthejesh <142912576+gitthejesh@users.noreply.github.com>
2025-04-28 10:57:03 +0530

    Updated app.js with new message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Native App!');
+  res.send('🚀 Hello from Updated Cloud Native App!');
 });
 
 app.listen(port, () => {
